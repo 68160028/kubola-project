c35c107b3dc76fbff007356f9bab9e4df6e46373
68160028 <68160028@go.buu.ac.th>
2026-03-30 23:09:42 +0700

    Update GameObject.java

--- Paws Path Home/src/GameObject.java
+++ Paws Path Home/src/GameObject.java
@@ -6,11 +6,11 @@ public abstract class GameObject {
     public GameObject(int x, int y, int w, int h){
         this.x = x; this.y = y;
         this.width = w; this.height = h;
-    }
+    } //ตำแหน่ง + ขนาด
 
     public Rectangle getBounds(){
         return new Rectangle(x, y, width, height);
-    }
+    } //ตรวจการชน
 
     public abstract void update();
     public abstract void draw(Graphics g);
