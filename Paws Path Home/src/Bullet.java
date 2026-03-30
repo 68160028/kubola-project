836b5083308a2b05e0ba82d4c24b1dca6513fb97
68160028 <68160028@go.buu.ac.th>
2026-03-30 23:05:28 +0700

    Update Bullet.java

--- Paws Path Home/src/Bullet.java
+++ Paws Path Home/src/Bullet.java
@@ -2,24 +2,24 @@ import java.awt.*;
 
 public class Bullet extends GameObject {
     private int speed;
-    private boolean alive = true;
+    private boolean alive = true; //สถานะ
 
     public Bullet(int x,int y,boolean right){
         super(x,y,20,10);
         speed = right ? 8 : -8;
-    }
+    } //ทิศการยิง
 
-    public boolean isAlive(){ return alive; }
+    public boolean isAlive(){ return alive; } //เช็คสถานะกระสุน
 
     @Override
     public void update(){
         x += speed;
         if(x < -50 || x > 900) alive = false;
-    }
+    } //ลบกระสุนออกถ้าหลุดจอ
 
     @Override
     public void draw(Graphics g){
-        if(alive){
+        if(alive){ //แสดงรูปถ้ายังอยู่ในจอ
             g.setColor(Color.RED);
             g.fillRect(x,y,width,height);
         }
