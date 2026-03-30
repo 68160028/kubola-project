4561e4ee4e9783c74f2f7906fa014a445e01f897
68160028 <68160028@go.buu.ac.th>
2026-03-30 23:14:19 +0700

    Update Monster.java

--- Paws Path Home/src/Monster.java
+++ Paws Path Home/src/Monster.java
@@ -5,29 +5,29 @@ import java.net.URL;
 public class Monster extends GameObject implements Movable {
 
     private Image img;
-    private int dir=1;
+    private int dir=1; //ทิศการเดิน
     private boolean alive=true;
 
     public Monster(int x,int y){
         super(x,y,60,60);
         URL u=getClass().getResource("/assets/mons.png");
         img=new ImageIcon(u).getImage();
-    }
+    } //โหลดรูป
 
     public boolean isAlive(){return alive;}
-    public void die(){alive=false;}
+    public void die(){alive=false;} //เช็คสถานะ
 
     @Override
     public void update(){
-        if (!alive) return;
+        if (!alive) return; //เดินแค่ตอนยังอยู่
         x+=dir*2;
-        if(x<0||x>740) dir*=-1;
+        if(x<0||x>740) dir*=-1; //ถ้าชนให้กลับทิศ
     }
 
     @Override
     public void draw(Graphics g){
         if(alive) g.drawImage(img,x,y,width,height,null);
-    }
+    } //แสดงรูป
 
     @Override
     public void move() {
